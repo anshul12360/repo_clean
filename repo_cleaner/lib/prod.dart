5844d84b93d37f48dc96cc2df9ad8a012665f3d8
anshul12360 <anshul12360@gmail.com>
2025-01-22 10:00:07 +0530

    dev inital

---
+++ repo_cleaner/lib/prod.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Prod extends StatefulWidget {
+  const Prod({super.key});
+
+  @override
+  State<Prod> createState() => _ProdState();
+}
+
+class _ProdState extends State<Prod> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Prod");
+  }
+}
