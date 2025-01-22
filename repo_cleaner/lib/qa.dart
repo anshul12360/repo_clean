2fc3ca2a2fc832986525c76014fa10bdaa591cba
anshul12360 <anshul12360@gmail.com>
2025-01-22 10:02:33 +0530

    qa inital

---
+++ repo_cleaner/lib/qa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Qa extends StatefulWidget {
+  const Qa({super.key});
+
+  @override
+  State<Qa> createState() => _QaState();
+}
+
+class _QaState extends State<Qa> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Qa");
+  }
+}
