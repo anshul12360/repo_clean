d82026b4f8930e1df3f035bc891d668fd7c2caca
anshul12360 <anshul12360@gmail.com>
2025-01-22 10:02:33 +0530

    qa inital

---
+++ repo_cleaner/lib/qa.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ***REMOVED*** extends StatefulWidget {
+  const ***REMOVED***({super.key});
+
+  @override
+  State<***REMOVED***> createState() => _***REMOVED***State();
+}
+
+class _***REMOVED***State extends State<***REMOVED***> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("***REMOVED***");
+  }
+}
