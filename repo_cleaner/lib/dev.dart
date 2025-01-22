164bd075e0b951e2003bc746d99eb1f67762b319
anshul12360 <anshul12360@gmail.com>
2025-01-22 09:58:01 +0530

    dev inital

---
+++ repo_cleaner/lib/dev.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dev extends StatefulWidget {
+  const Dev({super.key});
+
+  @override
+  State<Dev> createState() => _DevState();
+}
+
+class _DevState extends State<Dev> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Dev");
+  }
+}
